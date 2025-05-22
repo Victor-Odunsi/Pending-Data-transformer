2682cff8e156285a61d4fafaa3eb32b6ceaa9523
Victor Odunsi <141575954+Victor-Odunsi@users.noreply.github.com>
2025-05-22 13:19:01 +0100

    Update Pending Transformer fxn.py

--- Pending Transformer fxn.py
+++ Pending Transformer fxn.py
@@ -16,7 +16,7 @@ def Pending_transformer(filepath, sheet_name = 'Sheet1'):
     
     chem_keys = [
         r'U/E+CR', 'UREA', 'LIPASE', 'E/U/CR', 'LIPO', 'FBS', 'GGT', 'CAL', 'PO4', 'LFT', 'SOD', 'CRP', 'LIPOGRAM', 'CREAT', 'S',
-        'CHOL', 'CKMM', 'CR', 'CKMB', 'POT', 'ELEC', 'ALK', 'UA', 'CPK', 'MAG', 'CRP', 'LDH', 'BILIRUBIN', 'AFP', 'AMYL', 'GLU',
+        'CHOL', 'CKMM', 'CR', 'POT', 'ELEC', 'ALK', 'UA', 'CPK', 'MAG', 'CRP', 'LDH', 'BILIRUBIN', 'AFP', 'AMYL', 'GLU',
         'GLOBULIN', 'TOT PROT', 'ALB', 'GGT', 'ALDOLASE', 'ALT', 'AST', 'TBIL', 'ANION', 'DBIL', 'TRIG', 'IRON', 'CO2', 'URE', 'GLUCOSE',
         'VIT', 'ANTI-MULLERIAN', 'TESTO', 'T3', 'T4', 'CA153', 'PSA', 'bHCG', 'QHCG', 'E2', 'FOL', 'PROG', 'FSH', 'LH', 'PROL', 'TSH',
         'THY', 'CA125', 'TRANSF', 'SAT', 'FERR', 'CORT', 'CEA', 'CA19-9'
@@ -24,7 +24,7 @@ def Pending_transformer(filepath, sheet_name = 'Sheet1'):
 
     haem_keys = [
         'FBC', 'RETIC', 'GENOTYPE', 'BLOOD', 'COOMBS', 'HBAIC', 'ESR', 'MALARIA', 'RETICULOCYTES', 'APTT', 'PI', 'INR', 'QUANTIFERONE',
-        'Hct', 'HCT', 'TROPONIN', 'TROPO', 'DIMER', 'PLATELETS', 'NORMOBLASTS', 'CKMB', 'MICROFILARIA'
+        'Hct', 'HCT', 'TROPONIN', 'TROPO', 'DIMER', 'PLATELETS', 'NORMOBLASTS', 'CKMB', 'MICROFILARIA', 'MP'
     ]
 
     sero_keys = [
@@ -32,6 +32,11 @@ def Pending_transformer(filepath, sheet_name = 'Sheet1'):
     ]
     
     def classify(test_name):
+        for key in sero_keys:
+            match = re.search(key, test_name, re.IGNORECASE)
+            if match:
+                return 'serology'
+                
         for key in chem_keys:
             match = re.search(key, test_name, re.IGNORECASE)
             if match:
@@ -42,15 +47,10 @@ def Pending_transformer(filepath, sheet_name = 'Sheet1'):
             if match:
                 return 'haematology'
 
-        for key in sero_keys:
-            match = re.search(key, test_name, re.IGNORECASE)
-            if match:
-                return 'serology'
-
         return 'Others'
     
     df['TEST CATEGORY'] = df['TEST ORDERED'].apply(lambda test: classify(test))
     
     return df
 
-df.to_excel('pendings excel MAY 7th.xlsx', index = False)
+df.to_excel('Pendings excel MAY 7th.xlsx', index = False)
